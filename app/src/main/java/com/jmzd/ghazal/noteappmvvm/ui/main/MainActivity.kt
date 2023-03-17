0e97be6bd756de83b78510976bcbde4806a25aa7
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-03-17 06:33:45 -0700

    Note ViewModel

--- app/src/main/java/com/jmzd/ghazal/noteappmvvm/ui/main/MainActivity.kt
+++ app/src/main/java/com/jmzd/ghazal/noteappmvvm/ui/main/MainActivity.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import com.jmzd.ghazal.noteappmvvm.R
 import com.jmzd.ghazal.noteappmvvm.databinding.ActivityMainBinding
 import com.jmzd.ghazal.noteappmvvm.ui.main.note.NoteFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private var _binding : ActivityMainBinding? = null
---
+++ app/src/main/java/com/jmzd/ghazal/noteappmvvm/viewmodel/NoteViewModel.kt
@@ -0,0 +1,32 @@
+package com.jmzd.ghazal.noteappmvvm.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.jmzd.ghazal.noteappmvvm.data.model.NoteEntity
+import com.jmzd.ghazal.noteappmvvm.utils.*
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class NoteViewModel @Inject constructor() : ViewModel() {
+    //spinners
+    val categoriesList = MutableLiveData<MutableList<String>>()
+    val prioritiesList = MutableLiveData<MutableList<String>>()
+
+    /*
+    * اگر lunch() را به این صورت بنویسیم هم درست است و به صورت پیش فرض روی ترد main میرود
+    * ولی میخوایم کار با تردها و جا به جا شدن بین ترد ها را یاد بگیریم پس خودمون بهش Dispatchers هم میدیم
+    * */
+    fun loadCategoriesData() = viewModelScope.launch(Dispatchers.IO) {
+        val data = mutableListOf(WORK, EDUCATION, HOME, HEALTH)
+        categoriesList.postValue(data)
+    }
+
+    fun loadPrioritiesData() = viewModelScope.launch(Dispatchers.IO) {
+        val data = mutableListOf(HIGH, NORMAL, LOW)
+        prioritiesList.postValue(data)
+    }
+}
