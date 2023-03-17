9974329b72650b237fe3e5742b8bafa119ce0360
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-03-17 06:08:17 -0700

    bindings

--- app/src/main/java/com/jmzd/ghazal/noteappmvvm/ui/main/MainActivity.kt
+++ app/src/main/java/com/jmzd/ghazal/noteappmvvm/ui/main/MainActivity.kt
@@ -3,10 +3,31 @@ package com.jmzd.ghazal.noteappmvvm.ui.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.jmzd.ghazal.noteappmvvm.R
+import com.jmzd.ghazal.noteappmvvm.databinding.ActivityMainBinding
+import com.jmzd.ghazal.noteappmvvm.ui.main.note.NoteFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding : ActivityMainBinding? = null
+    private val binding get() = _binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        //init views
+        binding?.apply {
+            //Note fragment
+            addNoteBtn.setOnClickListener {
+                NoteFragment().show(supportFragmentManager, NoteFragment().tag)
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
--- app/src/main/java/com/jmzd/ghazal/noteappmvvm/ui/main/note/NoteFragment.kt
+++ app/src/main/java/com/jmzd/ghazal/noteappmvvm/ui/main/note/NoteFragment.kt
@@ -5,22 +5,37 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.jmzd.ghazal.noteappmvvm.databinding.FragmentNoteBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
 class NoteFragment : BottomSheetDialogFragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    //binding
+    private var _binding : FragmentNoteBinding? = null
+    private val binding get() = _binding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+    ): View {
+        _binding = FragmentNoteBinding.inflate(layoutInflater)
+        return binding!!.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //init views
+        binding?.apply {
+
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        _binding = null
     }
 
 }
