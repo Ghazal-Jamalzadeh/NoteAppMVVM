02d91ae0d4db2b86ab562e702916f7d6c58d1d77
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-03-17 09:44:23 -0700

    spinner extensions

--- app/src/main/java/com/jmzd/ghazal/noteappmvvm/ui/main/note/NoteFragment.kt
+++ app/src/main/java/com/jmzd/ghazal/noteappmvvm/ui/main/note/NoteFragment.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.jmzd.ghazal.noteappmvvm.databinding.FragmentNoteBinding
+import com.jmzd.ghazal.noteappmvvm.utils.setupListWithAdapter
+import com.jmzd.ghazal.noteappmvvm.viewmodel.NoteViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -15,6 +18,13 @@ class NoteFragment : BottomSheetDialogFragment() {
     //binding
     private var _binding : FragmentNoteBinding? = null
     private val binding get() = _binding
+
+    //Other
+    private val viewModel: NoteViewModel by viewModels()
+    private val categoriesList: MutableList<String> = mutableListOf()
+    private val prioriesList: MutableList<String> = mutableListOf()
+    private var category = ""
+    private var priority = ""
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,6 +39,19 @@ class NoteFragment : BottomSheetDialogFragment() {
         super.onViewCreated(view, savedInstanceState)
         //init views
         binding?.apply {
+
+            //Spinner Category
+            viewModel.loadCategoriesData()
+            viewModel.categoriesList.observe(viewLifecycleOwner) {
+                categoriesList.addAll(it)
+                categoriesSpinner.setupListWithAdapter(it) { itItem -> category = itItem }
+            }
+            //Spinner priority
+            viewModel.loadPrioritiesData()
+            viewModel.prioritiesList.observe(viewLifecycleOwner) {
+                prioriesList.addAll(it)
+                prioritySpinner.setupListWithAdapter(it) { itItem -> priority = itItem }
+            }
 
         }
     }
--- app/src/main/java/com/jmzd/ghazal/noteappmvvm/utils/Extensions.kt
+++ app/src/main/java/com/jmzd/ghazal/noteappmvvm/utils/Extensions.kt
@@ -1,2 +1,22 @@
 package com.jmzd.ghazal.noteappmvvm.utils
 
+import android.R
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+
+fun Spinner.setupListWithAdapter(list: MutableList<String>, callback: (String) -> Unit) {
+    val adapter = ArrayAdapter(context, R.layout.simple_spinner_item, list)
+    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+    this.adapter = adapter
+    this.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+        override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+            callback(list[p2])
+        }
+
+        override fun onNothingSelected(p0: AdapterView<*>?) {
+
+        }
+    }
+}
